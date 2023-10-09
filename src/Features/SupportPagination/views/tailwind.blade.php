8d50016d66f713c71e614cc34c20ac8635a9a082
luk-sz <123759104+luk-sz@users.noreply.github.com>
2023-10-09 23:46:41 +0200

    add spaces between words
    Phrase Showing{$paginator->firstItem()}to{$paginator->lastItem()}of{$paginator->total()}results had no spaces between words, looks much better with spaces.

--- src/Features/SupportPagination/views/tailwind.blade.php
+++ src/Features/SupportPagination/views/tailwind.blade.php
@@ -41,7 +41,7 @@ $scrollIntoViewJsSnippet = ($scrollTo !== false)
 
             <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
                 <div>
-                    <p class="text-sm text-gray-700 leading-5">
+                    <p class="text-sm text-gray-700 leading-5 flex space-x-1">
                         <span>{!! __('Showing') !!}</span>
                         <span class="font-medium">{{ $paginator->firstItem() }}</span>
                         <span>{!! __('to') !!}</span>
